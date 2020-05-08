eee19ab7beb645b2ea5ad98efd20f2948b3bb9a6
Migas <miguel_cunha_99@hotmail.com>
2020-05-08 02:09:07 +0100

    Adicionadas novas dependências e mais alguma documentação

--- RestApi/app.js
+++ RestApi/app.js
@@ -3,8 +3,14 @@ require('dotenv/config');
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var swaggerUi = require('swagger-ui-express');
 var swaggerDocument = require('./swagger.json')
+
+//Middlewares
+app.use(cors());
+app.use(bodyParser.json());
 
 //Import Routes
 const apiRouter = require('./routes');
@@ -22,9 +28,9 @@ app.get("/", function(req, res){
 app.use('/api', apiRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-//Middlewares
-//app.use(auth);
-
 //Initialize the server
 app.listen(process.env.DEFAULT_PORT);
-console.log("http://localhost:3000");
+
+//Additional information
+console.log("Server is listenning on: http://localhost:3000 .");
+console.log("Check http://localhost:3000/api-docs for additional information.");
